1523cbb4ee7580c678f94b3bcdc46c63eda27a51
krupnov <d.l.krupnov@gmail.com>
2022-03-21 18:16:25 +0100

    5 заданий за день. Неплохо

---
+++ 1.6 - 10s.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+with webdriver.Chrome() as browser:
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Мой код, который заполняет обязательные поля
+    first_name_input = browser.find_element(By.CSS_SELECTOR, ".form-control.first:required")
+    first_name_input.send_keys("Мой ответ")
+    last_name_input = browser.find_element(By.CSS_SELECTOR, ".form-control.second:required")
+    last_name_input.send_keys("Мой ответ")
+    email_input = browser.find_element(By.CSS_SELECTOR, ".form-control.third:required")
+    email_input.send_keys("Мой ответ")
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
---
+++ 2.1 - 5s.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+with webdriver.Chrome() as browser:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/math.html")
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    browser.find_element(By.ID, "answer").send_keys(y)
+    browser.find_element(By.ID, "robotCheckbox").click()
+    browser.find_element(By.ID, "robotsRule").click()
+    browser.find_element(By.CSS_SELECTOR, ".btn.btn-default").click()
