ff6a9c1194effff8d9fcfc31e07d6235a97ddaa7
krupnov <d.l.krupnov@gmail.com>
2022-03-22 20:18:04 +0100

    2 задания за день. Лучше бы 5

---
+++ 2.1 - 7s.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+with webdriver.Chrome() as browser:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/get_attribute.html")
+    x_element = browser.find_element(By.ID, "treasure")
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+    browser.find_element(By.ID, "answer").send_keys(y)
+    browser.find_element(By.ID, "robotCheckbox").click()
+    browser.find_element(By.ID, "robotsRule").click()
+    browser.find_element(By.CSS_SELECTOR, ".btn.btn-default").click()
---
+++ 2.2 - 3s.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+
+with webdriver.Chrome() as browser:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/selects1.html")
+    num1 = int(browser.find_element(By.ID, "num1").text)
+    num2 = int(browser.find_element(By.ID, "num2").text)
+    Select(browser.find_element(By.CSS_SELECTOR, ".custom-select")).select_by_value(str(num1 + num2))
+    browser.find_element(By.CSS_SELECTOR, ".btn.btn-default").click()
+    time.sleep(5)
