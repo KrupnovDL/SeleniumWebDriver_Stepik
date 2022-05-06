94594f845c52b48bdce824ddef82d0585b5c964e
krupnov <d.l.krupnov@gmail.com>
2022-05-06 10:36:19 +0200

    added webdriver_manager

--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(ChromeDriverManager().install())
     browser.implicitly_wait(7)
     yield browser
     print("\nquit browser..")
