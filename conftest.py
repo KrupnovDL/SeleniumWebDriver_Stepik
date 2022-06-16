a9da68176998b6588acb01547c4e809dbabd6b94
KrupnovDL <d.l.krupnov@gmail.com>
2022-06-16 12:19:01 +0200

    Now I can choose chrome of firefox

--- conftest.py
+++ conftest.py
@@ -2,13 +2,32 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+import os
+
+
+os.environ['GH_TOKEN'] = "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"
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store", default="chrome",
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def driver():
-    print("\nstart browser for test..")
-    s = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s)
+def driver(request):
+    browser_name = request.config.getoption("browser_name")
+    driver = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        s = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=s)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        s = Service(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=s)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     driver.maximize_window()
     driver.implicitly_wait(7)
     yield driver
