7a115781aeb965f6a7ef6f827cb61c9bbab53be9
KrupnovDL <d.l.krupnov@gmail.com>
2022-07-14 12:06:54 +0200

    added is_element_present in BasePage, added should_be_login_link in MainPage, added test_guest_should_see_login_link in test_main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
@@ -5,3 +8,10 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
